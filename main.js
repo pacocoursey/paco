ebf6195db080cb1c4e4feedce3565e713cc2dc48
pacocoursey <pvco.coursey@gmail.com>
2019-02-18 13:56:12 -0700

    Fix theme switcher, animate menu

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* global window, document, localStorage */
+
 let theme = localStorage.getItem('theme') || 'black';
 
 function setTheme(color, bg) {
@@ -5,15 +7,37 @@ function setTheme(color, bg) {
   document.documentElement.style.setProperty('--color', color);
 }
 
-window.addEventListener('load', () => {
+function initTheme() {
   const toggle = document.querySelector('.toggle');
+
   toggle.addEventListener('click', () => {
     if (theme === 'black') {
-      setTheme('--black', '--white');
+      setTheme('#111', '#fdfdfd');
       theme = 'white';
+      localStorage.setItem('theme', theme);
     } else {
-      setTheme('--white', '--black');
+      setTheme('#fdfdfd', '#111');
       theme = 'black';
+      localStorage.setItem('theme', theme);
     }
   });
+}
+
+function initMenu() {
+  const menu = document.querySelector('.menu');
+  const burger = document.querySelector('.burger');
+  const isVisible = false;
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('visible');
+  });
+}
+
+if (theme === 'white') {
+  setTheme('#111', '#fdfdfd');
+}
+
+window.addEventListener('load', () => {
+  initTheme();
+  initMenu();
 });
