3e9a4689ea3f2e4d5cef88ec6f517ba4cfc20951
pacocoursey <pvco.coursey@gmail.com>
2019-02-18 18:56:51 -0700

    Styling improvements to blog

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 /* global window, document, localStorage */
 
 let theme = localStorage.getItem('theme') || 'black';
+const white = '#fdfdfd';
+const black = '#111';
 
-function setTheme(color, bg) {
-  document.documentElement.style.setProperty('--bg', bg);
-  document.documentElement.style.setProperty('--color', color);
+function setTheme(isWhite = false) {
+  if (isWhite) {
+    document.documentElement.style.setProperty('--bg', white);
+    document.documentElement.style.setProperty('--color', black);
+    document.documentElement.style.setProperty('--light-gray', '#f0f0f0');
+    document.documentElement.style.setProperty('--gray', '#7f7f7f');
+  } else {
+    document.documentElement.style.setProperty('--bg', black);
+    document.documentElement.style.setProperty('--color', white);
+    document.documentElement.style.setProperty('--light-gray', '#333');
+    document.documentElement.style.setProperty('--gray', '#666');
+  }
 }
 
 function initTheme() {
@@ -12,11 +23,11 @@ function initTheme() {
 
   toggle.addEventListener('click', () => {
     if (theme === 'black') {
-      setTheme('#111', '#fdfdfd');
+      setTheme(true);
       theme = 'white';
       localStorage.setItem('theme', theme);
     } else {
-      setTheme('#fdfdfd', '#111');
+      setTheme(false);
       theme = 'black';
       localStorage.setItem('theme', theme);
     }
