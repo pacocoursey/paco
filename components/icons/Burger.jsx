c3e0979618795135c8d8e79ac7be89ca7834d35d
pacocoursey <pvco.coursey@gmail.com>
2019-02-21 21:10:20 -0700

    Extract icons to their own components

--- components/icons/Burger.jsx
+++ components/icons/Burger.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <svg width="20px" viewBox="0 0 131 106" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="25" viewBox="0 0 131 106" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 8H130.5" stroke="black" strokeWidth="10" />
     <path d="M0 53H130.5" stroke="black" strokeWidth="10" />
     <path d="M0 98H130.5" stroke="black" strokeWidth="10" />
--- components/icons/Logo.jsx
+++ components/icons/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 390 462" width="20px" fill="none">
-    <path d="M15 375s0 72 72 72 72-72 72-72V172.5H15V375z" stroke="#111" strokeWidth="30" fill="#fff" />
-    <path d="M15 87s0-72 72-72h144s144 0 144 144-144 144-144 144h-72s-144 0-144 72V87z" fill="#fff" stroke="#111" strokeWidth="30" />
+  <svg width="30" viewBox="0 0 390 462" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15 375C15 375 15 447 87 447C159 447 159 375 159 375V303V172.5H15V375Z" stroke="#111" strokeWidth="20" />
+    <path d="M15 87C15 87 15 15 87 15H231C231 15 375 15 375 159C375 303 231 303 231 303H159C159 303 15 303 15 375V87Z" fill="white" stroke="#111" strokeWidth="20" />
   </svg>
 );
