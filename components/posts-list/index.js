8ffb7e93304c149322b13f94f8ce1c3d1ca3938b
pacocoursey <pvco.coursey@gmail.com>
2020-01-27 17:27:57 -0700

    Paginate posts lists

--- components/posts-list/index.js
+++ components/posts-list/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import cn from 'classnames'
 
 import Link from '../link'
@@ -22,35 +23,58 @@ data
       posts[displayDate].push(p)
     }
   })
+const entries = Object.entries(posts)
+const len = entries.length
+
+const P = ({ content, slug }) => {
+  return content.map(([month, list], j) => {
+    return (
+      <div key={`${month}-${list.length}-${j}`} className={styles.group}>
+        <time dateTime={new Date(month).toISOString()} className={styles.month}>
+          {month}
+        </time>
+
+        <div className={styles.posts}>
+          {list.map((post, i) => {
+            return (
+              <Link
+                href="/[post]"
+                as={`/${post.slug}`}
+                key={`${post.title}-${i}`}
+                className={cn(styles.post, {
+                  [styles.active]: post.slug === slug
+                })}
+              >
+                <div className={styles.title}>{post.title}</div>
+                <div className={cn(styles.description, 'clamp-2')}>
+                  {post.description}
+                </div>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    )
+  })
+}
 
 const Posts = ({ slug }) => {
+  const [showMore, setShowMore] = useState(3)
+
   return (
     <div className={styles.container}>
-      {Object.entries(posts).map(([month, list], j) => {
-        return (
-          <div key={`${month}-${list.length}-${j}`} className={styles.group}>
-            <date className={styles.month}>{month}</date>
-
-            <div className={styles.posts}>
-              {list.map((post, i) => {
-                return (
-                  <Link
-                    href="/[post]"
-                    as={`/${post.slug}`}
-                    key={`${post.title}-${i}`}
-                    className={cn(styles.post, {
-                      [styles.active]: post.slug === slug
-                    })}
-                  >
-                    <div className={styles.title}>{post.title}</div>
-                    <div className={cn(styles.description, 'clamp-2')}>{post.description}</div>
-                  </Link>
-                )
-              })}
-            </div>
-          </div>
-        )
-      })}
+      <P content={entries.slice(0, 3)} slug={slug} />
+      {showMore > 0 && <P content={entries.slice(3, showMore)} slug={slug} />}
+      {showMore < len && (
+        <button
+          onClick={() => {
+            setShowMore(showMore + 3)
+          }}
+          className={styles.button}
+        >
+          Show More
+        </button>
+      )}
     </div>
   )
 }
