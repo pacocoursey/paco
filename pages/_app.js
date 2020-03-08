17d246f37337d70f18596193c1c9e15b80f2a1b6
pacocoursey <pvco.coursey@gmail.com>
2020-03-08 01:03:53 -0700

    Add nprogress, remove transitions when toggling theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import Head from 'next/head'
+import Router from 'next/router'
 import App from 'next/app'
+import nprogress from 'nprogress'
+import debounce from 'lodash.debounce'
+
+// Only show nprogress after 500ms (slow loading)
+const start = debounce(nprogress.start, 500)
+Router.events.on('routeChangeStart', start)
+Router.events.on('routeChangeComplete', url => {
+  start.cancel()
+  nprogress.done()
+})
+Router.events.on('routeChangeError', () => {
+  start.cancel()
+  nprogress.done()
+})
 
 import '../styles/global.css'
 
