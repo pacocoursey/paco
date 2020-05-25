2e639377a851b647d94b7430557e5970d2049396
paco <pvco.coursey@gmail.com>
2020-05-25 15:31:57 -0600

    improve text entry styling

--- components/entry/text.js
+++ components/entry/text.js
@@ -3,22 +3,25 @@ import cn from 'classnames'
 
 import styles from './text.module.css'
 
-const TextEntry = ({ title, description, href }) => {
+const TextEntry = ({ title, description, type, comment, href }) => {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      title={`${title} (${description})`}
-      className={styles.link}
-    >
-      <div>
-        <p className={cn(styles.title, 'clamp')}>{title}</p>
-        {description && (
-          <p className={cn(styles.description, 'clamp')}>{description}</p>
-        )}
-      </div>
-    </a>
+    <li className={styles.item}>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={`${title} (${description})`}
+        className={styles.link}
+      >
+        <div className={styles.type}>{type}</div>
+        <div>
+          <p className={cn(styles.title, 'clamp')}>{title}</p>
+          {description && (
+            <p className={cn(styles.description, 'clamp')}>{description}</p>
+          )}
+        </div>
+      </a>
+    </li>
   )
 }
 
--- pages/reading.js
+++ pages/reading.js
@@ -1,32 +1,34 @@
 import Page from '@components/page'
-import Links from '@components/links'
 import Entry from '@components/entry/text'
 
 // Data
 import useData from '@lib/use-data'
-import { data, schema } from '@data/reading.json'
+import { data } from '@data/reading.json'
 
 const Reading = () => {
-  const { items, filter } = useData(data)
+  const { items } = useData(data)
 
   return (
     <Page
       title="Reading"
-      content={<Links schema={schema} filter={filter} />}
       description="Collection of articles, blog posts, and videos that I enjoyed."
     >
       <article>
-        {items.map(entry => {
-          return (
-            <Entry
-              key={entry.title}
-              title={entry.title}
-              image={entry.image}
-              href={entry.url}
-              description={entry.description}
-            />
-          )
-        })}
+        <ul>
+          {items.map(entry => {
+            return (
+              <Entry
+                key={entry.title}
+                title={entry.title}
+                image={entry.image}
+                href={entry.url}
+                type={entry.key}
+                comment={entry.comment}
+                description={entry.description}
+              />
+            )
+          })}
+        </ul>
       </article>
     </Page>
   )
