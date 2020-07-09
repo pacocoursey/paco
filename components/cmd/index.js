2e65709346bb701b01b32bbccbed0bee17cca85c
paco <34928425+pacocoursey@users.noreply.github.com>
2020-07-09 17:31:15 -0600

    split checkpoint

---
+++ components/cmd/index.js
@@ -0,0 +1,179 @@
+import { useEffect, createContext, useContext, useRef, forwardRef } from 'react'
+import { useId } from '@reach/auto-id'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
+import {
+  createDescendantContext,
+  useDescendantsInit,
+  DescendantProvider,
+  useDescendant
+} from '@reach/descendants'
+
+const DescendantContext = createDescendantContext('List')
+const CommandContext = createContext({})
+const useCommand = () => useContext(CommandContext)
+
+export function Command({
+  children,
+  'aria-label': label,
+  open,
+  onDismiss,
+  active,
+  setActive,
+  className,
+  overlayClassName
+}) {
+  const listId = useId()
+
+  const context = {
+    listId,
+    label,
+    active,
+    setActive
+  }
+
+  return (
+    <CommandContext.Provider value={context}>
+      <DialogOverlay
+        isOpen={open}
+        className={overlayClassName}
+        data-command-overlay=""
+        onDismiss={onDismiss}
+      >
+        <DialogContent className={className} data-command="" aria-label={label}>
+          {children}
+        </DialogContent>
+      </DialogOverlay>
+    </CommandContext.Provider>
+  )
+}
+
+const inputs = ['select', 'button', 'textarea']
+
+export function CommandList({ children }) {
+  const { listId, active, setActive } = useCommand()
+  const [descendants, setDescendants] = useDescendantsInit()
+
+  useEffect(() => {
+    function handleKey(e) {
+      switch (e.key) {
+        case 'ArrowDown': {
+          if (active < descendants.length - 1) {
+            setActive(active + 1)
+          }
+          break
+        }
+        case 'ArrowUp': {
+          if (active > 0) {
+            setActive(active - 1)
+          }
+          break
+        }
+        case 'Enter': {
+          const cb = descendants[active]?.callback
+
+          if (!cb) {
+            return
+          }
+
+          if (document.activeElement) {
+            // Ignore [Enter] when button, select, textarea, or contentEditable is focused
+            if (
+              inputs.indexOf(document.activeElement.tagName.toLowerCase()) !==
+                -1 ||
+              document.activeElement.contentEditable === 'true'
+            ) {
+              return
+            }
+
+            // Ignore [Enter] on inputs that aren't a CommandInput
+            if (!document.activeElement.hasAttribute('data-command-input')) {
+              return
+            }
+          }
+
+          cb()
+          break
+        }
+        default:
+          break
+      }
+    }
+
+    window.addEventListener('keydown', handleKey)
+    return () => window.removeEventListener('keydown', handleKey)
+  }, [active, descendants, setActive, children])
+
+  return (
+    <>
+      <ul role="listbox" id={listId} data-command-list="">
+        <DescendantProvider
+          context={DescendantContext}
+          items={descendants}
+          set={setDescendants}
+        >
+          {children}
+        </DescendantProvider>
+      </ul>
+
+      {descendants.length > 0 && (
+        <div aria-live="polite" role="status" data-command-list-results="">
+          {descendants.length} results available.
+        </div>
+      )}
+    </>
+  )
+}
+
+export function CommandItem({ children, callback }) {
+  const ref = useRef()
+  const { active, setActive } = useCommand()
+
+  const index = useDescendant(
+    {
+      element: ref.current,
+      callback
+    },
+    DescendantContext
+  )
+
+  const isActive = active === index
+
+  return (
+    <li
+      ref={ref}
+      onClick={callback}
+      // TODO: debounce this
+      onMouseMove={() => setActive(index)}
+      style={{
+        color: isActive ? '#fff' : '#888'
+      }}
+      // a11y
+      tabIndex={-1}
+      aria-selected={isActive || undefined}
+      role="option"
+      data-command-item=""
+    >
+      {children}
+    </li>
+  )
+}
+
+export const CommandInput = forwardRef(({ ...props }, ref) => {
+  const { listId, label } = useCommand()
+
+  return (
+    <input
+      ref={ref}
+      {...props}
+      type="text"
+      // a11y
+      aria-expanded={true}
+      aria-autocomplete="list"
+      autoComplete="off"
+      role="combobox"
+      aria-label={label}
+      aria-owns={listId}
+      data-command-input=""
+    />
+  )
+})
--- pages/inverse.js
+++ pages/inverse.js
@@ -1,84 +1,201 @@
+import { useEffect, useReducer, useState, useRef } from 'react'
 import {
-  useState,
-  useMemo,
-  Children,
-  cloneElement,
-  useEffect,
-  createContext,
-  useContext
-} from 'react'
+  Command,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from '@components/cmd'
 import matchSorter from 'match-sorter'
-import { useId } from '@reach/auto-id'
-import { Dialog } from '@reach/dialog'
+import cn from 'classnames'
 
 import Page from '@components/page'
+import styles from '@styles/inverse.module.css'
+import Button from '@components/button'
+import useDelayedRender from 'use-delayed-render'
 
-const blogItems = [
-  <CommandItem value="Post A">Post A</CommandItem>,
-  <CommandItem value="Post B">Post B</CommandItem>
-]
+const Label = ({ children }) => {
+  return <li className={styles.label}>{children}</li>
+}
 
-const priceItems = value => [
-  <CommandItem>{value * 0.88} Euros</CommandItem>,
-  <CommandItem>{value * 0.8} Pound sterling</CommandItem>,
-  <CommandItem>{value * 22.36} Mexican Pesos</CommandItem>,
-  <CommandItem>{value * 16.06} Egyptian Pound</CommandItem>
-]
-
-const defaultItems = (setScreens, search) => [
-  <CommandItem value="Toggle Theme" callback={() => alert('Toggle theme')}>
-    Toggle Theme
+const Group = ({ children, title }) => {
+  return (
+    <>
+      <Label>{title}</Label>
+      {children}
+    </>
+  )
+}
+const BlogItems = () => [
+  <CommandItem value="Post A" key="Post A">
+    Post A
   </CommandItem>,
-  <CommandItem
-    value="Search Blog"
-    callback={() => setScreens(screens => [...screens, blogItems])}
-  >
-    Search blog
-  </CommandItem>,
-  <CommandItem
-    value="Calculate"
-    callback={() => {
-      setScreens(screens => [...screens, priceItems(Number(search))])
-    }}
-  >
-    Calculate Tax
+  <CommandItem value="Post B" key="Post B">
+    Post B
   </CommandItem>
 ]
 
+const PriceItems = ({ state: { search } }) => {
+  const value = Number(search)
+
+  return [
+    <CommandItem key="euros">{value * 0.88} Euros</CommandItem>,
+    <CommandItem key="pounds">{value * 0.8} Pound sterling</CommandItem>,
+    <CommandItem key="pesos">{value * 22.36} Mexican Pesos</CommandItem>,
+    <CommandItem key="egypt">{value * 16.06} Egyptian Pound</CommandItem>
+  ]
+}
+
+const DefaultItems = ({ state, dispatch }) => {
+  const [checked, setChecked] = useState(false)
+
+  const items = [
+    <CommandItem
+      value="Toggle Theme"
+      key="Toggle Theme"
+      callback={() => alert('Toggle theme')}
+    >
+      Toggle Theme
+    </CommandItem>,
+    <CommandItem
+      value="Search Blog"
+      key="Search Blog"
+      callback={() =>
+        dispatch({ type: 'setItems', items: [...state.items, BlogItems] })
+      }
+    >
+      Search blog
+    </CommandItem>,
+    <CommandItem
+      value="Calculate"
+      key="Calculate"
+      callback={() =>
+        dispatch({ type: 'setItems', items: [...state.items, PriceItems] })
+      }
+    >
+      Calculate Tax
+    </CommandItem>,
+    <Group key="collection-1" title="HELLO">
+      <CommandItem value="Navigation 1">Navigation 1</CommandItem>
+      <CommandItem value="Navigation 2">Navigation 2</CommandItem>
+      <CommandItem value="Navigation 3">Navigation 3</CommandItem>
+    </Group>,
+    <CommandItem
+      value="Check me"
+      key="Check me"
+      callback={() => setChecked(!checked)}
+    >
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      Check me
+    </CommandItem>
+  ]
+
+  return matchSorter(items, state.search, { keys: ['props.value'] })
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'pop': {
+      if (state.items.length > 1) {
+        return { ...state, items: state.items.slice(0, -1) }
+      }
+
+      return state
+    }
+    case 'toggle': {
+      return { ...state, open: !state.open }
+    }
+    case 'setActive': {
+      if (state.active === action.active) {
+        return state
+      }
+
+      return { ...state, active: action.active }
+    }
+    case 'setSearch': {
+      return { ...state, search: action.value }
+    }
+    case 'setItems': {
+      return { ...state, items: action.items }
+    }
+    default:
+      throw new Error(`Invalid action.type: ${action.type}`)
+  }
+}
+
 const Test = () => {
-  const [search, setSearch] = useState('')
-  const [active, setActive] = useState(0)
-  const [open, setOpen] = useState(true)
-  const [screens, setScreens] = useState(() => [defaultItems(() => {}, search)])
+  const [state, dispatch] = useReducer(reducer, {
+    items: [DefaultItems],
+    search: '',
+    active: 0,
+    open: true
+  })
+  const { search, active, open, items } = state
+  const { mounted, rendered } = useDelayedRender(open, {
+    enterDelay: -1,
+    exitDelay: 200
+  })
+  const inputRef = useRef()
 
-  // const filterItems = useMemo(() => {
-  //   // const items = screens[screens.length - 1]
-
-  //   // return matchSorter(items, search, { keys: ['props.value'] })
-  // }, [screens, search, mode])
-
-  const filterItems = screens[screens.length - 1]
+  const Items = items[items.length - 1]
 
   useEffect(() => {
-    setActive(0)
-  }, [search])
+    // When search changes or item set changes
+    dispatch({ type: 'setActive', active: 0 })
+  }, [state.search, Items])
 
-  // useEffect(() => {
-  //   setSearch('')
-  // }, [mode])
+  useEffect(() => {
+    dispatch({ type: 'setSearch', value: '' })
+    inputRef.current?.focus()
+  }, [Items])
+
+  useEffect(() => {
+    if (!open) {
+      dispatch({ type: 'setActive', active: 0 })
+      dispatch({ type: 'setItems', items: [DefaultItems] })
+    }
+  }, [open])
 
   return (
     <Page title="Command">
       <h1>Command Testing</h1>
-      <button onClick={() => setOpen(!open)}>Toggle</button>
+      <button onClick={() => dispatch({ type: 'toggle' })}>Toggle</button>
 
-      <Command aria-label="Navigation Menu" open={open}>
-        <CommandInput
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        <CommandList active={active} setActive={setActive}>
-          {filterItems}
+      <Command
+        open={mounted}
+        aria-label="Navigation Menu"
+        active={active}
+        setActive={active => dispatch({ type: 'setActive', active })}
+        className={cn(styles.command, {
+          [styles.show]: rendered
+        })}
+        overlayClassName={cn(styles.screen, {
+          [styles.show]: rendered
+        })}
+        onDismiss={() => dispatch({ type: 'toggle' })}
+      >
+        <div className={styles.top}>
+          <Button
+            onClick={() => dispatch({ type: 'pop' })}
+            disabled={items.length === 1}
+          >
+            ←
+          </Button>
+          <CommandInput
+            ref={inputRef}
+            value={search}
+            onChange={e =>
+              dispatch({ type: 'setSearch', value: e.target.value })
+            }
+            placeholder="Search..."
+          />
+        </div>
+
+        <CommandList>
+          <Items state={state} dispatch={dispatch} />
         </CommandList>
       </Command>
     </Page>
@@ -86,113 +203,3 @@ const Test = () => {
 }
 
 export default Test
-
-const CommandContext = createContext({})
-const useCommand = () => useContext(CommandContext)
-
-function Command({ children, 'aria-label': label, open }) {
-  const listId = useId()
-
-  const context = {
-    listId,
-    label
-  }
-
-  return (
-    <CommandContext.Provider value={context}>
-      <Dialog isOpen={open}>{children}</Dialog>
-    </CommandContext.Provider>
-  )
-}
-
-function CommandList({ children, active, setActive }) {
-  const { listId } = useCommand()
-  const childCount = Children.count(children)
-
-  useEffect(() => {
-    function handleKey(e) {
-      switch (e.key) {
-        case 'ArrowDown': {
-          if (active < childCount - 1) {
-            setActive(active + 1)
-          }
-          break
-        }
-        case 'ArrowUp': {
-          if (active > 0) {
-            setActive(active - 1)
-          }
-          break
-        }
-        case 'Enter': {
-          const cb = Children.toArray(children)[active].props.callback
-          if (cb) {
-            cb()
-          }
-          break
-        }
-        default:
-          break
-      }
-    }
-
-    window.addEventListener('keydown', handleKey)
-    return () => window.removeEventListener('keydown', handleKey)
-  }, [active, childCount, setActive, children])
-
-  return (
-    <>
-      <ul role="listbox" id={listId}>
-        {Children.map(children, (child, i) => {
-          return cloneElement(child, {
-            active: i === active,
-            setActive: () => setActive(i)
-          })
-        })}
-      </ul>
-
-      {childCount > 0 && (
-        <div aria-live="polite" role="status">
-          {childCount} results available.
-        </div>
-      )}
-    </>
-  )
-}
-
-function CommandItem({ children, active, setActive, callback }) {
-  return (
-    <li
-      onClick={callback}
-      onMouseMove={setActive}
-      style={{
-        color: active ? '#fff' : '#888'
-      }}
-      // a11y
-      tabIndex={-1}
-      aria-selected={active || undefined}
-      role="option"
-    >
-      {children}
-    </li>
-  )
-}
-
-function CommandInput({ value, onChange }) {
-  const { listId, label } = useCommand()
-
-  return (
-    <input
-      value={value || undefined}
-      onChange={onChange}
-      type="text"
-      // a11y
-      aria-expanded={true}
-      aria-autocomplete="list"
-      autoComplete="off"
-      role="combobox"
-      aria-label={label}
-      aria-owns={listId}
-    />
-  )
-}
