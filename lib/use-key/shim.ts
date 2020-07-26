3344a52f80fbdd99bd275eba9d7624e237f58e44
paco <34928425+pacocoursey@users.noreply.github.com>
2020-07-26 15:04:29 -0600

    Build stuff

--- lib/use-key/shim.ts
+++ lib/use-key/shim.ts
@@ -1,41 +1,44 @@
 // Based on https://github.com/shvaikalesh/shim-keyboard-event-key
 // but with SSR support
 
-(function() {
-  "use strict"
+// @ts-ignore
+;(function() {
+  'use strict'
 
   if (typeof self === 'undefined') return
 
   const event = KeyboardEvent.prototype
-  const desc = Object.getOwnPropertyDescriptor(event, "key")
+  const desc = Object.getOwnPropertyDescriptor(event, 'key')
   if (!desc) return
 
   const keys = {
-    Win: "Meta",
-    Scroll: "ScrollLock",
-    Spacebar: " ",
+    Win: 'Meta',
+    Scroll: 'ScrollLock',
+    Spacebar: ' ',
 
-    Down: "ArrowDown",
-    Left: "ArrowLeft",
-    Right: "ArrowRight",
-    Up: "ArrowUp",
+    Down: 'ArrowDown',
+    Left: 'ArrowLeft',
+    Right: 'ArrowRight',
+    Up: 'ArrowUp',
 
-    Del: "Delete",
-    Apps: "ContextMenu",
-    Esc: "Escape",
+    Del: 'Delete',
+    Apps: 'ContextMenu',
+    Esc: 'Escape',
 
-    Multiply: "*",
-    Add: "+",
-    Subtract: "-",
-    Decimal: ".",
-    Divide: "/",
+    Multiply: '*',
+    Add: '+',
+    Subtract: '-',
+    Decimal: '.',
+    Divide: '/'
   }
 
-  Object.defineProperty(event, "key", {
+  Object.defineProperty(event, 'key', {
     get: function() {
+      // @ts-ignore
       const key = desc.get.call(this)
 
+      // @ts-ignore
       return keys.hasOwnProperty(key) ? keys[key] : key
-    },
+    }
   })
 })()
