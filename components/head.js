61bf0fbba1098e3df68e11d5f45fce75bccdb159
paco <34928425+pacocoursey@users.noreply.github.com>
2020-10-14 21:05:40 -0600

    bring back inter

--- components/head.js
+++ components/head.js
@@ -15,13 +15,13 @@ const Head = ({
   return (
     <NextHead>
       {/* Preload font */}
-      {/* <link
+      <link
         rel="preload"
         href="https://assets.vercel.com/raw/upload/v1587415301/fonts/2/inter-var-latin.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
-      /> */}
+      />
 
       {/* Title */}
       <title>{title}</title>
