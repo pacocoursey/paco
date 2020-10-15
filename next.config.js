a728475b1eebbacc674429b46a2749859f585700
paco <34928425+pacocoursey@users.noreply.github.com>
2020-10-14 20:50:25 -0600

    try out preact

--- next.config.js
+++ next.config.js
@@ -1,5 +1,49 @@
-module.exports = {
+const withPrefresh = require('@prefresh/next')
+
+const config = {
   typescript: {
     ignoreDevErrors: true
+  },
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true
+  },
+
+  webpack(config, { dev, isServer }) {
+    const splitChunks = config.optimization && config.optimization.splitChunks
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules
+        })
+        cacheGroups.commons.name = 'framework'
+      } else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules
+        }
+      }
+    }
+
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {})
+    aliases.react = aliases['react-dom'] = 'preact/compat'
+
+    // inject Preact DevTools
+    if (dev && !isServer) {
+      const entry = config.entry
+      config.entry = () =>
+        entry().then(entries => {
+          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || [])
+          return entries
+        })
+    }
+
+    return config
   }
 }
+
+module.exports = withPrefresh(config)
